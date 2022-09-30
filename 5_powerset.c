cfced5d22016956d611b184c913195f3b9fd8de6
Kunal Koushik <71688599+KunalKoushik@users.noreply.github.com>
2022-09-30 21:32:58 +0530

    PRO LEVEL by KK

--- 5_powerset.c
+++ 5_powerset.c
@@ -1,51 +1,42 @@
 #include<stdio.h>
-#include<math.h>
-void powerset(int *set1, int* set2,int *s1,int *s2){
-    int element;
-    int i,j;
-    set2[0] = NULL;
-    for ( i = 0; i < *s2; i++)
-    {
-        set2[i+1] = set1[i] ;
-        element = set1[i];   
-    }
-    *s2 = i+1;
-    for ( i = 0; i < *s2; i++)
-    {
-        for ( j = 0; i < *s2; j++)
-        {
-            
-        }
-          
-    }
-    
-
-    
-}
-
-void display(int *U, int size)
-{
-    printf("Power Set is : ");
-    for (int i = 0; i < size; i++)
-    {
-        printf("%d\t",U[i]);
-    }
-}
 int main()
 {
-    int s1, s2;
-    int set1[100000], set2[100000] ;
-
-    printf("Enter size of set 1 : ");
-    scanf("%d", &s1);
-    for (int i = 0; i < s1; i++)
-    {
-        printf("Enter %d element :", i);
-        scanf("%d", &set1[i]);
-    }
-    s2 = pow(2,s1);
-    
-    display(set2, s2);
-
-    return 0;
+	int i,set[5];
+	for(i=0;i<4;i++)
+	{
+		printf("Enter elements of set = ");
+		scanf("%d",&set[i]);
+	}
+	printf("{Fie");
+	for(i=0;i<4;i++)
+	{	
+	printf(",{%d}",set[i]);
+	}
+	for(i=0;i<3;i++)
+	{
+	printf(",{%d,%d}",set[0],set[i+1]);
+	}
+	for(i=0;i<2;i++)
+	{
+	printf(",{%d,%d}",set[1],set[i+2]);
+	}
+	for(i=0;i<1;i++)
+	{
+	printf(",{%d,%d}",set[2],set[i+3]);
+	}
+	for(i=0;i<2;i++)
+	{
+	printf(",{%d,%d,%d}",set[0],set[1],set[i+2]);
+	}
+		for(i=0;i<2;i++)
+	{
+	printf(",{%d,%d,%d}",set[i],set[2],set[3]);
+	}
+	printf("{");
+		for(i=0;i<4;i++)
+	{	
+	printf("%d,",set[i]);
+	}
+printf("}}");
+return 0;
 }
